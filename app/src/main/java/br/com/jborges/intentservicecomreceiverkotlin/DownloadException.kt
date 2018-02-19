72610925fda21efa519359a7fc908ee76fdf8d63
jeffersonborges <34252717+jeffersonborges@users.noreply.github.com>
2018-02-18 23:47:36 -0300

    Criação do projeto

---
+++ app/src/main/java/br/com/jborges/intentservicecomreceiverkotlin/DownloadException.kt
@@ -0,0 +1,7 @@
+package br.com.jborges.intentservicecomreceiverkotlin
+
+/**
+ * Created by jeffe on 18/02/2018.
+ */
+class DownloadException(message: String) : Exception(message) {
+}
---
+++ app/src/main/java/br/com/jborges/intentservicecomreceiverkotlin/DownloadResultReceiver.kt
@@ -0,0 +1,26 @@
+package br.com.jborges.intentservicecomreceiverkotlin
+
+/**
+ * Created by jeffe on 18/02/2018.
+ */
+import android.os.Bundle
+import android.os.Handler
+import android.os.ResultReceiver
+
+class DownloadResultReceiver(handler: Handler) : ResultReceiver(handler) {
+    private var mReceiver: Receiver? = null
+
+    fun setReceiver(receiver: Receiver) {
+        mReceiver = receiver
+    }
+
+    interface Receiver {
+        fun onReceiveResult(resultCode: Int, resultData: Bundle)
+    }
+
+    override fun onReceiveResult(resultCode: Int, resultData: Bundle) {
+        if (mReceiver != null) {
+            mReceiver!!.onReceiveResult(resultCode, resultData)
+        }
+    }
+}
